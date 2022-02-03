b0b284f2bb7f3ea1bb1f149abe21b7f7a4300726
Xiao Wang <wang3702@purdue.edu>
2022-02-03 22:54:07 +0800

    add scholr fetch

---
+++ php_scholar/pub.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>My Publication</title>
+</head>
+<body>
+  <?php  $lang="en"; $userid="AGS_dK8AAAAJ"; $pagesize="100";
+include 'curl.php'; ?>
+<?php  include 'citation_chart.php'; ?>
+</body>
+</html>
